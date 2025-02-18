6cf9cd5d806ef2f2730c68cf3bed2eaf1526e30d
ryl1k <146999658+ryl1k@users.noreply.github.com>
2025-02-18 21:40:01 +0200

    Add project files.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace bunker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
